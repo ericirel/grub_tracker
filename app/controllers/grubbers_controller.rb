cc702cabc2bcc0df439367a96bf8a82be90a80c7
eric-irel <eric.herskovic@gmail.com>
2014-10-16 11:28:37 -0400

    Add email messaging to model

--- app/controllers/grubbers_controller.rb
+++ app/controllers/grubbers_controller.rb
@@ -1,26 +1,10 @@
 class GrubbersController < ApplicationController
+  require 'mandrill'
 
   def message
     @message = params[:body]
     #send messages!
-    puts "****** Sending Messages ******"
-    require 'mandrill'
-    m = Mandrill::API.new
-    message = {
-      :subject=> "Grub alert!",
-      :from_name=> "Grub Tracker",
-      :text=> @message,
-      :to=>[
-      {
-      :email=> "tehsheepy@gmail.com",
-      :name=> "Eric Herskovic"
-    }
-  ],
-    :html=>"<html><h1>Hi <strong>#{@message}</strong>, Grub Tracker</h1></html>",
-    :from_email=>"sender@yourdomain.com"
-    }
-    sending = m.messages.send message
-    puts sending
+    Grubber.email_grubbers(message)
   end
 
   def index
--- app/models/grubber.rb
+++ app/models/grubber.rb
@@ -1,5 +1,7 @@
 class Grubber < ActiveRecord::Base
-  scope :subscribed, ->{where(subscribed: true)}
+  scope :subscribed, ->{ where(subscribed: true) }
+  scope :emailable, -> { where(email_ok: true, subscribed: true) }
+  scope :textable, -> { subscribed.where(text_ok: true) }
   validates :password, presence: true
   before_create :configure_new_grubber
 
@@ -10,6 +12,43 @@ class Grubber < ActiveRecord::Base
     end
     if self.email.present?
        self.email_ok = true
+    end
+
+
+  def send_email(message_body)
+    m = Mandrill::API.new
+    recipient = [email]
+    message = {
+      :subject=> "Grub alert!",
+      :from_name=> "Grub Tracker",
+      :text=> message_body,
+      :to=> recipient,
+      :html=>"<html><h1><strong>#{message_body}</strong>, Grub Tracker</h1></html>",
+      :from_email=>"tehsheepy@gmail.com"
+      }
+      sending = m.messages.send message
+      puts sending
+    end
+
+  end
+
+  def self.email_grubbers(message_body)
+    grubber_emails = Grubber.emailable.map do |grubber|
+      Hash(email: grubber.email)
+    end
+    emailable_grubbers.each do |email|
+      recipient = [email]
+      m = Mandrill::API.new
+      message = {
+        :subject=> "Grub alert!",
+        :from_name=> "Grub Tracker",
+        :text=> message_body,
+        :to=> recipient,
+        :html=>"<html><h1><strong>#{message_body}</strong>, Grub Tracker</h1></html>",
+        :from_email=>"tehsheepy@gmail.com"
+      }
+      sending = m.messages.send message
+      puts sending
     end
   end
 end
