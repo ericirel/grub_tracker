995215751daf080e7ae3dc405be3f3bcd31dc683
eric-irel <eric.herskovic@gmail.com>
2014-10-16 14:14:07 -0400

    Fixed status on Twilio Controller

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,11 +1,12 @@
 class TwilioController < ApplicationController
 
+  skip_before_action :verify_authenticity_token
 
   def sms
     @message = params[:Body]
     Grubber.email_grubbers(message)
     Grubber.text_grubbers(message)
-    render status :ok
+    render status: :ok
   end
 
 end
